1c882325b892a7038e8b5b1e0c2d9cb481735321
huzaifa <huzaifainshal@gmail.com>
2026-03-01 15:20:56 +0500

    `Update` - in project section side projects link added

--- sections/projects/projects.section.tsx
+++ sections/projects/projects.section.tsx
@@ -4,6 +4,10 @@ import { cn } from "@/utils/cn.util";
 import { projectsData } from "@/config-and-data/projects.data";
 import ProjectItem from "./section-components/project-item.component";
 import { styles_SectionContainer } from "@/styles/section/section.styles";
+import { styles_Typography } from "@/styles/typography/typography.styles";
+import Link from "next/link";
+import { userData } from "@/config-and-data/user.data";
+import UnderlinedText from "@/components/underlined-text.component";
 
 const ProjectsSection = () => {
   return (
@@ -15,6 +19,10 @@ const ProjectsSection = () => {
           {projectsData.map((p, i) => (
             <ProjectItem key={p.id} item={p} index={i} />
           ))}
+        </div>
+
+        <div className="flex items-center justify-center w-full mt-10">
+          <p className={cn(styles_Typography['heading-3'])}>Check my <UnderlinedText><Link target='_blank' href={userData.socials.github}>Github</Link></UnderlinedText> to view all my side projects.</p>
         </div>
       </div>
     </section>
